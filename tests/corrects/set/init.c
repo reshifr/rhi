684fb06782d5af11e7402172d94f323d41171cb7
Renol P. H <reshifr@gmail.com>
2021-04-08 18:47:00 +0700

    Add a description

--- tests/corrects/set/init.c
+++ tests/corrects/set/init.c
@@ -3,24 +3,30 @@
 void set_init(struct rhifunc func) {
   struct rhis* set;
 
+#if 1
+  /* Initial set with fixed */
   HANDLE((set=rhis_init(&func,
     RHI_FIXED))==NULL, "Set init failed.");
   set_print(set);
   rhis_free(set);
 
+  /* Initial set with shrink */
   HANDLE((set=rhis_init(&func,
     RHI_SHRINK))==NULL, "Set init failed.");
   set_print(set);
   rhis_free(set);
 
+  /* Initial set with extend */
   HANDLE((set=rhis_init(&func,
     RHI_EXTEND))==NULL, "Set init failed.");
   set_print(set);
   rhis_free(set);
 
+  /* Initial set with shrink and extend */
   HANDLE((set=rhis_init(&func,
     RHI_SHRINK|RHI_EXTEND))==NULL, "Set init failed.");
   set_print(set);
   rhis_free(set);
+#endif
 }
 
