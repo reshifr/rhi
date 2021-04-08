58ebdee81acb380e3d01a9dbb0a482ff99d9213b
Renol P. H <reshifr@gmail.com>
2021-04-09 01:11:22 +0700

    Added performance test modules

--- tests/set/insert.c
+++ tests/set/insert.c
@@ -1,16 +1,18 @@
-#include "../set.h"
+#include "../tests.h"
 
-void set_insert(struct rhifunc func) {
+void set_insert(struct rhifunc* func) {
   struct rhis* set;
   struct objs* keys;
+  int64_t start;
 
-#if 0
-  /* Initial with fixed */
-  HANDLE((set=rhis_init(&func,
+/* Consistency tests for `rhi_init` */
+#if 1
+  /* Fixed mode */
+  HANDLE((set=rhis_init(func,
     RHI_FIXED))==NULL, "Set init failed.");
   /* Initial list of keys */
-  HANDLE((keys=objs_init(MIN_OBJLEN, MAX_OBJLEN,
-    NUM_KEYS, OBJCASE))==NULL, "Keys init failed.");
+  HANDLE((keys=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN,
+    CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init failed.");
   /* Insert keys */
   for(rhiuint i=0; i<keys->count; ++i)
     ASSERT(!rhis_insert(set, keys->objs[i]),
@@ -22,12 +24,12 @@ void set_insert(struct rhifunc func) {
   rhis_free(set);
   objs_destroy(keys);
 
-  /* Initial with extend */
-  HANDLE((set=rhis_init(&func,
+  /* Extend mode */
+  HANDLE((set=rhis_init(func,
     RHI_EXTEND))==NULL, "Set init failed.");
   /* Initial list of keys */
-  HANDLE((keys=objs_init(MIN_OBJLEN, MAX_OBJLEN,
-    NUM_KEYS, OBJCASE))==NULL, "Keys init failed.");
+  HANDLE((keys=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN,
+    CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init failed.");
   /* Insert keys */
   for(rhiuint i=0; i<keys->count; ++i)
     ASSERT(!rhis_insert(set, keys->objs[i]),
@@ -39,9 +41,29 @@ void set_insert(struct rhifunc func) {
   rhis_free(set);
   objs_destroy(keys);
 
-  /* Initial with shrink and extend */
-  HANDLE((set=rhis_init(&func,
+  /* Shrink and extend mode */
+  HANDLE((set=rhis_init(func,
     RHI_SHRINK|RHI_EXTEND))==NULL, "Set init failed.");
+  /* Initial list of keys */
+  HANDLE((keys=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN,
+    CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init failed.");
+  /* Insert keys */
+  for(rhiuint i=0; i<keys->count; ++i)
+    ASSERT(!rhis_insert(set, keys->objs[i]),
+      "Insert %s failed.", (const char*)keys->objs[i]);
+  set_print(set);
+  /* Insert NULL */
+  ASSERT(!rhis_insert(set, NULL), "Insert %s failed.", "NULL");
+  set_print(set);
+  rhis_free(set);
+  objs_destroy(keys);
+#endif
+
+/* Consistency tests for `rhi_reserve` */
+#if 0
+  /* Reserve with fixed */
+  HANDLE((set=rhis_init(&func,
+    RHI_FIXED))==NULL, "Set init failed.");
   /* Initial list of keys */
   HANDLE((keys=objs_init(MIN_OBJLEN, MAX_OBJLEN,
     NUM_KEYS, OBJCASE))==NULL, "Keys init failed.");
@@ -57,21 +79,36 @@ void set_insert(struct rhifunc func) {
   objs_destroy(keys);
 #endif
 
-#if 1
-  /* Reserve with fixed */
-  HANDLE((set=rhis_init(&func,
-    RHI_FIXED))==NULL, "Set init failed.");
+/* Performance tests for `rhis_init` */
+#if 0
+  HANDLE((set=rhis_init(func,
+    RHI_SHRINK|RHI_EXTEND))==NULL, "Set init failed.");
   /* Initial list of keys */
-  HANDLE((keys=objs_init(MIN_OBJLEN, MAX_OBJLEN,
-    NUM_KEYS, OBJCASE))==NULL, "Keys init failed.");
+  HANDLE((keys=objs_init(PERF_MIN_OBJLEN, PERF_MAX_OBJLEN,
+    PERF_NUM_KEYS, PERF_OBJCASE))==NULL, "Keys init failed.");
   /* Insert keys */
+  start = get_clock();
   for(rhiuint i=0; i<keys->count; ++i)
-    ASSERT(!rhis_insert(set, keys->objs[i]),
-      "Insert %s failed.", (const char*)keys->objs[i]);
-  set_print(set);
-  /* Insert NULL */
-  ASSERT(!rhis_insert(set, NULL), "Insert %s failed.", "NULL");
-  set_print(set);
+    rhis_insert(set, keys->objs[i]);
+  printf("Insert time in `rhi_init`: %"PRId64" ms\n", get_clock()-start);
+  set_mprint(set);
+  rhis_free(set);
+  objs_destroy(keys);
+#endif
+
+/* Performance tests for `rhis_reserve` */
+#if 0
+  HANDLE((set=rhis_reserve(func, PERF_RESERVE_SIZE,
+    RHI_SHRINK|RHI_EXTEND))==NULL, "Set init failed.");
+  /* Initial list of keys */
+  HANDLE((keys=objs_init(PERF_MIN_OBJLEN, PERF_MAX_OBJLEN,
+    PERF_NUM_KEYS, PERF_OBJCASE))==NULL, "Keys init failed.");
+  /* Insert keys */
+  start = get_clock();
+  for(rhiuint i=0; i<keys->count; ++i)
+    rhis_insert(set, keys->objs[i]);
+  printf("Insert time in `rhis_reserve`: %"PRId64" ms\n", get_clock()-start);
+  set_mprint(set);
   rhis_free(set);
   objs_destroy(keys);
 #endif
