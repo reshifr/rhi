d5b97714ac389fd704d776251db0284d0056a091
Renol P. H <reshifr@gmail.com>
2021-04-08 19:53:42 +0700

    Add macros

--- tests/set.h
+++ tests/set.h
@@ -1,12 +1,11 @@
-#include "../utils.h"
+#include "utils.h"
 
 #define MIN_OBJLEN 3
 #define MAX_OBJLEN 5
+#define RESERVE_SIZE 10
+#define OBJCASE UPPER
+#define NUM_KEYS 16
 
 void set_init(struct rhifunc func);
 void set_reserve(struct rhifunc func);
 void set_insert(struct rhifunc func);
-
-void map_init(struct rhifunc func);
-void map_reserve(struct rhifunc func);
-void map_insert(struct rhifunc func);
