4b3f1ba32c3c4b70d0fd448a2025f59c76b8f54a
Renol P. H <reshifr@gmail.com>
2021-04-08 17:10:09 +0700

    Add `rhis_reserve` test cases

--- tests/corrects/set/reserve.c
+++ tests/corrects/set/reserve.c
@@ -3,30 +3,30 @@
 void set_reserve(struct rhifunc func) {
   struct rhis* set;
 
-#if 1
-  HANDLE((set=rhis_reserve(&func, 5,
+#if 0
+  HANDLE((set=rhis_reserve(&func, 10,
     RHI_FIXED))==NULL, "Set reserve failed.");
   set_print(set);
   rhis_free(set);
   
-  HANDLE((set=rhis_reserve(&func, 5,
+  HANDLE((set=rhis_reserve(&func, 10,
     RHI_SHRINK))==NULL, "Set reserve failed.");
   set_print(set);
   rhis_free(set);
   
-  HANDLE((set=rhis_reserve(&func, 5,
+  HANDLE((set=rhis_reserve(&func, 10,
     RHI_EXTEND))==NULL, "Set reserve failed.");
   set_print(set);
   rhis_free(set);
   
-  HANDLE((set=rhis_reserve(&func, 5,
+  HANDLE((set=rhis_reserve(&func, 10,
     RHI_SHRINK|RHI_EXTEND))==NULL, "Set reserve failed.");
   set_print(set);
   rhis_free(set);
 #endif
 
-#if 0
-  HANDLE((set=rhis_reserve(&func, 1000,
+#if 1
+  HANDLE((set=rhis_reserve(&func, 100,
     RHI_SHRINK|RHI_EXTEND))==NULL, "Set reserve failed.");
   set_mprint(set);
   rhis_free(set);
