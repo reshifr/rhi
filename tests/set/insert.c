3fdaed19d21cfc22b68ae7c00bded132181aad33
Renol P. H <reshifr@gmail.com>
2021-04-11 11:44:10 +0700

    Simplify test functions

--- tests/set/insert.c
+++ tests/set/insert.c
@@ -1,36 +1,54 @@
 #include "../tests.h"
 
-/* Using `rhi_init` */
-#define CONS_INIT_TESTS(func, mode, print) \
+#define INIT 0
+#define RESERVE 1
+
+#define CONS_TESTS(initial, func, mode, print) \
   do { \
+    bool r; \
     struct rhis* set; \
     struct objs* keys[2]; \
  \
-    /* Initial set */ \
-    HANDLE((set=rhis_init(func, mode))==NULL, "Set init failed."); \
+    /* initial set */ \
+    if( initial==INIT ) { \
+      /* using `rhi_init` */ \
+      HANDLE((set=rhis_init(func, mode))==NULL, "Set init failed."); \
+    } else if( initial==RESERVE ) { \
+      /* using `rhi_reserve` */ \
+      HANDLE((set=rhis_reserve(func, \
+        CONS_RESERVE_SIZE, mode))==NULL, "Set reserve failed."); \
+    } \
  \
-    /* Initial list of keys 1 */ \
+    /* [1] initial list of keys */ \
     HANDLE((keys[0]=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN, \
-      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init 1 failed."); \
-    /* Initial list of keys 2 */ \
+      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "[1] Keys init failed."); \
+    /* [2] initial list of keys */ \
     HANDLE((keys[1]=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN, \
-      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init 2 failed."); \
+      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "[2] Keys init failed."); \
  \
-    /* Insert keys 1 */ \
-    for(rhiuint i=0; i<keys[0]->count; ++i) \
-      ASSERT(!rhis_insert(set, keys[0]->objs[i]), \
-        "Insert %s 1 failed.", (const char*)keys[0]->objs[i]); \
+    /* [1] insert keys */ \
+    for(rhiuint i=0; i<keys[0]->count; ++i) { \
+      r = rhis_insert(set, keys[0]->objs[i]); \
+      ASSERT(r, "[1] Insert %s success.", (const char*)keys[0]->objs[i]); \
+      ASSERT(!r, "[1] Insert %s failed.", (const char*)keys[0]->objs[i]); \
+    } \
     print(set); \
-    /* Insert keys 2 */ \
-    for(rhiuint i=0; i<keys[1]->count; ++i) \
-      ASSERT(!rhis_insert(set, keys[1]->objs[i]), \
-        "Insert %s 2 failed.", (const char*)keys[1]->objs[i]); \
+    /* [2] insert keys */ \
+    for(rhiuint i=0; i<keys[1]->count; ++i) { \
+      r = rhis_insert(set, keys[1]->objs[i]); \
+      ASSERT(r, "[2] Insert %s success.", (const char*)keys[1]->objs[i]); \
+      ASSERT(!r, "[2] Insert %s failed.", (const char*)keys[1]->objs[i]); \
+    } \
     print(set); \
  \
-    /* Insert NULL 1 */ \
-    ASSERT(!rhis_insert(set, NULL), "Insert %s 1 failed.", "NULL"); \
-    /* Insert NULL 2*/ \
-    ASSERT(!rhis_insert(set, NULL), "Insert %s 2 failed.", "NULL"); \
+    /* [1] insert NULL */ \
+    r = rhis_insert(set, NULL); \
+    ASSERT(r, "[1] Insert %s success.", "NULL"); \
+    ASSERT(!r, "[1] Insert %s failed.", "NULL"); \
+    /* [2] insert NULL 2*/ \
+    r = rhis_insert(set, NULL); \
+    ASSERT(r, "[2] Insert %s success.", "NULL"); \
+    ASSERT(!r, "[2] Insert %s failed.", "NULL"); \
  \
     print(set); \
     rhis_free(set); \
@@ -38,91 +56,32 @@
     objs_destroy(keys[1]); \
   } while(0)
 
-/* Using `rhi_reserve` */
-#define CONS_RESERVE_TESTS(func, mode, print) \
-  do { \
-    struct rhis* set; \
-    struct objs* keys[2]; \
- \
-    /* Reserve set */ \
-    HANDLE((set=rhis_reserve(func, \
-      CONS_RESERVE_SIZE, mode))==NULL, "Set init failed."); \
- \
-    /* Initial list of keys 1 */ \
-    HANDLE((keys[0]=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN, \
-      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init 1 failed."); \
-    /* Initial list of keys 2 */ \
-    HANDLE((keys[1]=objs_init(CONS_MIN_OBJLEN, CONS_MAX_OBJLEN, \
-      CONS_NUM_KEYS, CONS_OBJCASE))==NULL, "Keys init 2 failed."); \
- \
-    /* Insert keys 1 */ \
-    for(rhiuint i=0; i<keys[0]->count; ++i) \
-      ASSERT(!rhis_insert(set, keys[0]->objs[i]), \
-        "Insert %s 1 failed.", (const char*)keys[0]->objs[i]); \
-    print(set); \
-    /* Insert keys 2 */ \
-    for(rhiuint i=0; i<keys[1]->count; ++i) \
-      ASSERT(!rhis_insert(set, keys[1]->objs[i]), \
-        "Insert %s 2 failed.", (const char*)keys[1]->objs[i]); \
-    print(set); \
- \
-    /* Insert NULL 1 */ \
-    ASSERT(!rhis_insert(set, NULL), "Insert %s 1 failed.", "NULL"); \
-    /* Insert NULL 2*/ \
-    ASSERT(!rhis_insert(set, NULL), "Insert %s 2 failed.", "NULL"); \
- \
-    print(set); \
-    rhis_free(set); \
-    objs_destroy(keys[0]); \
-    objs_destroy(keys[1]); \
-  } while(0)
-
-/* Using `rhi_init` */
-#define PERF_INIT_TESTS(func, mode, print) \
+#define PERF_TESTS(initial, func, mode, print) \
   do { \
     long start; \
     struct rhis* set; \
     struct objs* keys; \
  \
-    /* Initial set */ \
-    HANDLE((set=rhis_init(func, mode))==NULL, "Set init failed."); \
+    /* initial set */ \
+    if( initial==INIT ) { \
+      /* using `rhi_init` */ \
+      HANDLE((set=rhis_init(func, mode))==NULL, "Set init failed."); \
+    } else if( initial==RESERVE ) { \
+      /* using `rhi_reserve` */ \
+      HANDLE((set=rhis_reserve(func, \
+        PERF_RESERVE_SIZE, mode))==NULL, "Set reserve failed."); \
+    } \
  \
-    /* Initial list of keys */ \
+    /* initial list of keys */ \
     HANDLE((keys=objs_init(PERF_MIN_OBJLEN, PERF_MAX_OBJLEN, \
       PERF_NUM_KEYS, PERF_OBJCASE))==NULL, "Keys init failed."); \
  \
-    /* Insert keys */ \
+    /* initial keys */ \
     start = get_clock(); \
     for(rhiuint i=0; i<keys->count; ++i) \
       rhis_insert(set, keys->objs[i]); \
  \
-    printf("Insert time in `rhi_init`: %ld ms\n", get_clock()-start); \
-    print(set); \
-    rhis_free(set); \
-    objs_destroy(keys); \
-  } while(0)
-
-/* Using `rhi_reserve` */
-#define PERF_RESERVE_TESTS(func, mode, print) \
-  do { \
-    long start; \
-    struct rhis* set; \
-    struct objs* keys; \
- \
-    /* Initial set */ \
-    HANDLE((set=rhis_reserve(func, \
-      PERF_RESERVE_SIZE, mode))==NULL, "Set init failed."); \
- \
-    /* Initial list of keys */ \
-    HANDLE((keys=objs_init(PERF_MIN_OBJLEN, PERF_MAX_OBJLEN, \
-      PERF_NUM_KEYS, PERF_OBJCASE))==NULL, "Keys init failed."); \
- \
-    /* Insert keys */ \
-    start = get_clock(); \
-    for(rhiuint i=0; i<keys->count; ++i) \
-      rhis_insert(set, keys->objs[i]); \
- \
-    printf("Insert time in `rhi_init`: %ld ms\n", get_clock()-start); \
+    printf("Insert time: %ld ms\n", get_clock()-start); \
     print(set); \
     rhis_free(set); \
     objs_destroy(keys); \
@@ -131,23 +90,23 @@
 extern struct rhifunc func;
 
 void set_insert(void) {
-  TEST(CONS_INIT_TESTS(&func, RHI_FIXED, set_print), 0);
-  TEST(CONS_INIT_TESTS(&func, RHI_SHRINK, set_print), 0);
-  TEST(CONS_INIT_TESTS(&func, RHI_EXTEND, set_print), 0);
-  TEST(CONS_INIT_TESTS(&func, RHI_SHRINK|RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(INIT, &func, RHI_FIXED, set_print), 0);
+  TEST(CONS_TESTS(INIT, &func, RHI_SHRINK, set_print), 0);
+  TEST(CONS_TESTS(INIT, &func, RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(INIT, &func, RHI_SHRINK|RHI_EXTEND, set_print), 0);
 
-  TEST(CONS_RESERVE_TESTS(&func, RHI_FIXED, set_print), 0);
-  TEST(CONS_RESERVE_TESTS(&func, RHI_SHRINK, set_print), 0);
-  TEST(CONS_RESERVE_TESTS(&func, RHI_EXTEND, set_print), 0);
-  TEST(CONS_RESERVE_TESTS(&func, RHI_SHRINK|RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(RESERVE, &func, RHI_FIXED, set_print), 0);
+  TEST(CONS_TESTS(RESERVE,&func, RHI_SHRINK, set_print), 0);
+  TEST(CONS_TESTS(RESERVE,&func, RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(RESERVE,&func, RHI_SHRINK|RHI_EXTEND, set_print), 0);
 
-  TEST(PERF_INIT_TESTS(&func, RHI_FIXED, set_mprint), 0);
-  TEST(PERF_INIT_TESTS(&func, RHI_SHRINK, set_mprint), 0);
-  TEST(PERF_INIT_TESTS(&func, RHI_EXTEND, set_mprint), 0);
-  TEST(PERF_INIT_TESTS(&func, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
+  TEST(PERF_TESTS(INIT, &func, RHI_FIXED, set_mprint), 0);
+  TEST(PERF_TESTS(INIT, &func, RHI_SHRINK, set_mprint), 0);
+  TEST(PERF_TESTS(INIT, &func, RHI_EXTEND, set_mprint), 0);
+  TEST(PERF_TESTS(INIT, &func, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
 
-  TEST(PERF_RESERVE_TESTS(&func, RHI_FIXED, set_mprint), 0);
-  TEST(PERF_RESERVE_TESTS(&func, RHI_SHRINK, set_mprint), 0);
-  TEST(PERF_RESERVE_TESTS(&func, RHI_EXTEND, set_mprint), 0);
-  TEST(PERF_RESERVE_TESTS(&func, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
+  TEST(PERF_TESTS(RESERVE,&func, RHI_FIXED, set_mprint), 0);
+  TEST(PERF_TESTS(RESERVE,&func, RHI_SHRINK, set_mprint), 0);
+  TEST(PERF_TESTS(RESERVE,&func, RHI_EXTEND, set_mprint), 0);
+  TEST(PERF_TESTS(RESERVE,&func, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
 }
