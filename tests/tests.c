b46038ec1f399f1f16ee1ae214f991747f4c83fe
Renol P. H <reshifr@gmail.com>
2021-04-18 14:45:34 +0700

    test: did some test cases
    - rhis_search() test cases
    - rhis_kreplace() test cases

--- tests/tests.c
+++ tests/tests.c
@@ -26,8 +26,7 @@ int main(void) {
   TEST(set_reserve(), 0);
   TEST(set_insert(), 1);
   TEST(set_search(), 1);
-  
-  TEST(set_replace(), 0);
-  TEST(set_kreplace(), 0);
+  TEST(set_replace(), 1);
+  TEST(set_kreplace(), 1);
   return EXIT_SUCCESS;
 }
