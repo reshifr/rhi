cc0927a04816a39e68c5481ff85c6e8f2324490c
Renol P. H <reshifr@gmail.com>
2021-04-20 17:37:19 +0700

    refactor: fix the test cases on rhis_delete()
    - Improve consistency tests on CONS_TESTS().

--- tests/set/delete.c
+++ tests/set/delete.c
@@ -5,8 +5,7 @@
 
 #define CONS_TESTS(initial, funcs, mode, print) \
   do { \
-    const void* r; \
-    bool rbool; \
+    bool r; \
     struct rhis* set; \
     struct objs* keys[2]; \
  \
@@ -21,56 +20,84 @@
         CONS_RESERVE_SIZE, mode))==NULL, "Set reserve failed."); \
     } \
  \
-    /* [0] initialize the object list */ \
     keys[0] = objs_init(CONS_MIN_OBJLEN, \
       CONS_MAX_OBJLEN, CONS_NUM_KEYS, CONS_OBJCASE); \
  \
-    /* [1] initialize the object list */ \
     keys[1] = objs_init(CONS_MIN_OBJLEN, \
       CONS_MAX_OBJLEN, CONS_NUM_KEYS, CONS_OBJCASE); \
  \
-    /* [0] ksearch with the dictionary is not populated */ \
-    print(set); \
-    for(rhiuint i=0; i<keys[0]->count; ++i) { \
-      r = rhis_ksearch(set, keys[0]->objs[i]); \
-      ASSERT(r==NULL, "[0] Ksearch %s not found.", \
-        (const char*)keys[0]->objs[i]); \
-      ASSERT(r!=NULL, "[0] Ksearch %s found.", (const char*)keys[0]->objs[i]); \
-    } \
- \
-    /* [0] ksearch NULL */ \
-    rbool = rhis_search(set, NULL); \
-    r = rhis_ksearch(set, NULL); \
-    ASSERT(rbool && r==NULL, "[0] Ksearch %s found.", "NULL"); \
-    ASSERT(!rbool && r==NULL, "[0] Ksearch %s not found.", "NULL"); \
- \
-    /* populates the dictionary */ \
+    /* [0] populates the dictionary by key[0] and NULL */ \
     for(rhiuint i=0; i<keys[0]->count; ++i) \
       rhis_insert(set, keys[0]->objs[i]); \
- \
-    /* populate with NULL */ \
     rhis_insert(set, NULL); \
- \
-    /* [1] ksearch with the dictionary is populated */ \
     print(set); \
-    for(rhiuint i=0; i<keys[1]->count; ++i) { \
-      r = rhis_ksearch(set, keys[1]->objs[i]); \
-      ASSERT(r==NULL, "[1] Ksearch %s not found.", \
-        (const char*)keys[1]->objs[i]); \
-      ASSERT(r!=NULL, "[1] Ksearch %s found.", (const char*)keys[1]->objs[i]); \
+ \
+    /* [0] delete by keys[0] */ \
+    for(rhiuint i=0; i<keys[0]->count; ++i) { \
+      r = rhis_delete(set, keys[0]->objs[i]); \
+      ASSERT(r, "[0] Delete %s success.", (const char*)keys[0]->objs[i]); \
+      ASSERT(!r, "[0] Delete %s failed.", (const char*)keys[0]->objs[i]); \
     } \
  \
-    /* [1] ksearch NULL */ \
-    rbool = rhis_search(set, NULL); \
-    r = rhis_ksearch(set, NULL); \
-    ASSERT(rbool && r==NULL, "[1] Ksearch %s found.", "NULL"); \
-    ASSERT(!rbool && r==NULL, "[1] Ksearch %s not found.", "NULL"); \
+    /* [0] delete NULL */ \
+    r = rhis_delete(set, NULL); \
+    ASSERT(r, "[0] Delete %s success.", "NULL"); \
+    ASSERT(!r, "[0] Delete %s failed.", "NULL"); \
+    print(set); \
+ \
+    /* [1] populates the dictionary by key[0] and NULL */ \
+    for(rhiuint i=0; i<keys[0]->count; ++i) \
+      rhis_insert(set, keys[0]->objs[i]); \
+    rhis_insert(set, NULL); \
+    print(set); \
+ \
+    /* [1] delete by keys[1] */ \
+    for(rhiuint i=0; i<keys[1]->count; ++i) { \
+      r = rhis_delete(set, keys[1]->objs[i]); \
+      ASSERT(r, "[1] Delete %s success.", (const char*)keys[1]->objs[i]); \
+      ASSERT(!r, "[1] Delete %s failed.", (const char*)keys[1]->objs[i]); \
+    } \
+ \
+    /* [1] delete NULL */ \
+    r = rhis_delete(set, NULL); \
+    ASSERT(r, "[1] Delete %s success.", "NULL"); \
+    ASSERT(!r, "[1] Delete %s failed.", "NULL"); \
+ \
+    /* [2] delete NULL */ \
+    r = rhis_delete(set, NULL); \
+    ASSERT(r, "[2] Delete %s success.", "NULL"); \
+    ASSERT(!r, "[2] Delete %s failed.", "NULL"); \
+    print(set); \
  \
     rhis_free(set); \
     objs_destroy(keys[0]); \
     objs_destroy(keys[1]); \
   } while(0)
 
+extern struct funcs funcs;
+extern struct funcs kfuncs;
+
 void set_delete(void) {
-  return;
+  TEST(CONS_TESTS(INIT, funcs, RHI_FIXED, set_print), 1);
+  TEST(CONS_TESTS(INIT, funcs, RHI_SHRINK, set_print), 0);
+  TEST(CONS_TESTS(INIT, funcs, RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(INIT, funcs, RHI_SHRINK|RHI_EXTEND, set_print), 0);
+
+  TEST(CONS_TESTS(RESERVE, funcs, RHI_FIXED, set_print), 0);
+  TEST(CONS_TESTS(RESERVE, funcs, RHI_SHRINK, set_print), 0);
+  TEST(CONS_TESTS(RESERVE, funcs, RHI_EXTEND, set_print), 0);
+  TEST(CONS_TESTS(RESERVE, funcs, RHI_SHRINK|RHI_EXTEND, set_print), 0);
+
+  // TEST(PERF_TESTS(INIT, funcs, RHI_FIXED, set_mprint), 0);
+  // TEST(PERF_TESTS(INIT, funcs, RHI_SHRINK, set_mprint), 0);
+  // TEST(PERF_TESTS(INIT, funcs, RHI_EXTEND, set_mprint), 0);
+  // TEST(PERF_TESTS(INIT, funcs, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
+
+  // TEST(PERF_TESTS(RESERVE, funcs, RHI_FIXED, set_mprint), 0);
+  // TEST(PERF_TESTS(RESERVE, funcs, RHI_SHRINK, set_mprint), 0);
+  // TEST(PERF_TESTS(RESERVE, funcs, RHI_EXTEND, set_mprint), 0);
+  // TEST(PERF_TESTS(RESERVE, funcs, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
+
+  // TEST(MEMORY_TESTS(INIT, kfuncs, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
+  // TEST(MEMORY_TESTS(RESERVE, kfuncs, RHI_SHRINK|RHI_EXTEND, set_mprint), 0);
 }
