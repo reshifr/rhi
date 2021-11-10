08ce9c2a338b3f3c5ddd0254b7bc938d107340c0
Renol P. H <reshifr@gmail.com>
2021-11-10 20:11:17 +0700

    fix: sub test cases update: test cases for automatic testing

--- tests/set/insert.cpp
+++ tests/set/insert.cpp
@@ -3,13 +3,14 @@
 #define UNIT_INSERT() \
   do { \
     printf("\n"); \
-    rhis_insert(set, NULL); \
+    bool success = rhis_insert(set, NULL); \
+    printf("%sNULL ", success ? "@" : ""); \
     keys.load([&set](void* obj) { \
       bool success = rhis_insert(set, obj); \
       printf("%s%s ", success ? "@" : "", (const char*)obj); \
     }); \
     std::printf("\nuniqueness=%" PRIrhiu " count=%" PRIrhiu, \
-      keys.uniqueness, utils::UNIT_COUNT); \
+      keys.uniqueness, keys.count); \
     utils::print(set); \
   } while(0)
 
@@ -17,12 +18,11 @@ static void unit_init_test(int mode) {
   struct rhis* set;
   HANDLE((set=rhis_init(utils::hash, utils::equal,
     mode))==NULL, "Error: rhis_init() failed.");
-  utils::objs<utils::UNIT_CASE>
-    keys(utils::UNIT_MIN, utils::UNIT_MAX, utils::UNIT_COUNT);
-  // Insert
+  utils::objs<utils::UPPER> keys(1, 1, utils::UNIT_COUNT);
   UNIT_INSERT();
-  keys.reverse();
-  // Insert
+  keys.shuffle();
+  UNIT_INSERT();
+  keys.add(utils::UNIT_COUNT);
   UNIT_INSERT();
   rhis_free(set);
 }
@@ -31,26 +31,75 @@ static void unit_reserve_test(int mode) {
   struct rhis* set;
   HANDLE((set=rhis_reserve(utils::hash, utils::equal,
     utils::UNIT_RESERVE, mode))==NULL, "Error: rhis_reserve() failed.");
-  utils::objs<utils::UNIT_CASE>
-    keys(utils::UNIT_MIN, utils::UNIT_MAX, utils::UNIT_COUNT);
-  // Insert
+  utils::objs<utils::UPPER> keys(1, 1, utils::UNIT_COUNT);
   UNIT_INSERT();
-  keys.reverse();
-  // Insert
+  keys.shuffle();
+  UNIT_INSERT();
+  keys.add(utils::UNIT_COUNT);
   UNIT_INSERT();
   rhis_free(set);
 }
 
-#define PERF_INSERT(type) \
+#define AUTO_INSERT() \
+  do { \
+    rhis_insert(set, NULL); \
+    keys.load([&set](void* obj) { \
+      rhis_insert(set, obj); \
+    }); \
+    std::printf("\nuniqueness=%" PRIrhiu " count=%" PRIrhiu, \
+      keys.uniqueness, keys.count); \
+    utils::mprint(set); \
+    HANDLE((set->mode&RHI_FIXED) && \
+      set->occupied!=keys.uniqueness && set->occupied!=set->max, \
+      "Failed: rhis_insert() occupied in trouble."); \
+    HANDLE((set->mode&RHI_EXTEND) && set->occupied!=keys.uniqueness, \
+      "Failed: rhis_insert() occupied in trouble."); \
+  } while(0)
+
+static void auto_init_test(int mode) {
+  struct rhis* set;
+  HANDLE((set=rhis_init(utils::hash, utils::equal,
+    mode))==NULL, "Error: rhis_init() failed.");
+  utils::objs keys(3, 15, utils::AUTO_COUNT);
+  AUTO_INSERT();
+  keys.shuffle();
+  AUTO_INSERT();
+  HANDLE(!set->has_null_inserted,
+    "Failed: rhis_insert() has_null_inserted in trouble.");
+  keys.add(utils::AUTO_COUNT);
+  AUTO_INSERT();
+  HANDLE(!set->has_null_inserted,
+    "Failed: rhis_insert() has_null_inserted in trouble.");
+  rhis_free(set);
+}
+
+static void auto_reserve_test(int mode) {
+  struct rhis* set;
+  HANDLE((set=rhis_reserve(utils::hash, utils::equal,
+    utils::UNIT_RESERVE, mode))==NULL, "Error: rhis_reserve() failed.");
+  utils::timer clock;
+  utils::objs keys(3, 15, utils::AUTO_COUNT);
+  AUTO_INSERT();
+  keys.shuffle();
+  HANDLE(!set->has_null_inserted,
+    "Failed: rhis_insert() has_null_inserted in trouble.");
+  keys.add(utils::AUTO_COUNT);
+  AUTO_INSERT();
+  HANDLE(!set->has_null_inserted,
+    "Failed: rhis_insert() has_null_inserted in trouble.");
+  rhis_free(set);
+}
+
+#define PERF_INSERT() \
   do { \
     clock.start(); \
     keys.load([&set](void* obj) { \
       rhis_insert(set, obj); \
     }); \
-    std::printf("\n%s=%" PRId64 " ms\n", \
-      type, clock.result<utils::timer::mili>()); \
+    std::printf("\n%" PRId64 " milliseconds\n", \
+      clock.result<utils::timer::mili>()); \
     std::printf("uniqueness=%" PRIrhiu " count=%" PRIrhiu, \
-      keys.uniqueness, utils::PERF_COUNT); \
+      keys.uniqueness, keys.count); \
     utils::mprint(set); \
   } while(0)
 
@@ -59,12 +108,9 @@ static void perf_init_test(int mode) {
   HANDLE((set=rhis_init(utils::hash, utils::equal,
     mode))==NULL, "Error: rhis_init() failed.");
   utils::timer clock;
-  utils::objs<utils::PERF_CASE>
-    keys(utils::PERF_MIN, utils::PERF_MAX, utils::PERF_COUNT);
-  PERF_INSERT("insert");
-  keys.reverse();
-  // Insert
-  PERF_INSERT("reserve_insert");
+  utils::objs keys(3, 15, utils::AUTO_COUNT);
+  PERF_INSERT();
+  PERF_INSERT();
   rhis_free(set);
 }
 
@@ -73,30 +119,41 @@ static void perf_reserve_test(int mode) {
   HANDLE((set=rhis_reserve(utils::hash, utils::equal,
     utils::UNIT_RESERVE, mode))==NULL, "Error: rhis_reserve() failed.");
   utils::timer clock;
-  utils::objs<utils::PERF_CASE>
-    keys(utils::PERF_MIN, utils::PERF_MAX, utils::PERF_COUNT);
-  PERF_INSERT("insert");
-  keys.reverse();
-  // Insert
-  PERF_INSERT("reserve_insert");
+  utils::objs keys(3, 15, utils::AUTO_COUNT);
+  PERF_INSERT();
+  PERF_INSERT();
   rhis_free(set);
 }
 
-void set::insert(void) {
-  TEST(0, unit_init_test(RHI_FIXED));
-  TEST(0, unit_init_test(RHI_SHRINK));
-  TEST(0, unit_init_test(RHI_EXTEND));
-  TEST(0, unit_init_test(RHI_SHRINK|RHI_EXTEND));
-  TEST(0, unit_reserve_test(RHI_FIXED));
-  TEST(0, unit_reserve_test(RHI_SHRINK));
-  TEST(0, unit_reserve_test(RHI_EXTEND));
-  TEST(0, unit_reserve_test(RHI_SHRINK|RHI_EXTEND));
-  TEST(0, perf_init_test(RHI_FIXED));
-  TEST(0, perf_init_test(RHI_SHRINK));
-  TEST(0, perf_init_test(RHI_EXTEND));
+void set::unit_insert(void) {
+  TEST(1, unit_init_test(RHI_FIXED));
+  TEST(1, unit_init_test(RHI_SHRINK));
+  TEST(1, unit_init_test(RHI_EXTEND));
+  TEST(1, unit_init_test(RHI_SHRINK|RHI_EXTEND));
+  TEST(1, unit_reserve_test(RHI_FIXED));
+  TEST(1, unit_reserve_test(RHI_SHRINK));
+  TEST(1, unit_reserve_test(RHI_EXTEND));
+  TEST(1, unit_reserve_test(RHI_SHRINK|RHI_EXTEND));
+}
+
+void set::auto_insert(void) {
+  TEST(1, auto_init_test(RHI_FIXED));
+  TEST(1, auto_init_test(RHI_SHRINK));
+  TEST(1, auto_init_test(RHI_EXTEND));
+  TEST(1, auto_init_test(RHI_SHRINK|RHI_EXTEND));
+  TEST(1, auto_reserve_test(RHI_FIXED));
+  TEST(1, auto_reserve_test(RHI_SHRINK));
+  TEST(1, auto_reserve_test(RHI_EXTEND));
+  TEST(1, auto_reserve_test(RHI_SHRINK|RHI_EXTEND));
+}
+
+void set::perf_insert(void) {
+  TEST(1, perf_init_test(RHI_FIXED));
+  TEST(1, perf_init_test(RHI_SHRINK));
+  TEST(1, perf_init_test(RHI_EXTEND));
   TEST(1, perf_init_test(RHI_SHRINK|RHI_EXTEND));
-  TEST(0, perf_reserve_test(RHI_FIXED));
-  TEST(0, perf_reserve_test(RHI_SHRINK));
-  TEST(0, perf_reserve_test(RHI_EXTEND));
-  TEST(0, perf_reserve_test(RHI_SHRINK|RHI_EXTEND));
+  TEST(1, perf_reserve_test(RHI_FIXED));
+  TEST(1, perf_reserve_test(RHI_SHRINK));
+  TEST(1, perf_reserve_test(RHI_EXTEND));
+  TEST(1, perf_reserve_test(RHI_SHRINK|RHI_EXTEND));
 }
