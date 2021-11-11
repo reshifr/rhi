a6b7bd258c63846ae3e05244971ce696c13f5237
Renol P. H <reshifr@gmail.com>
2021-11-11 19:27:12 +0700

    update: unit testing for set search

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,6 +4,7 @@ int main(void) {
   TEST(1, set::unit_init());
   TEST(1, set::unit_reserve());
   TEST(1, set::unit_insert());
+  TEST(1, set::unit_search());
   TEST(1, set::auto_insert());
   TEST(1, set::perf_insert());
   return EXIT_SUCCESS;
