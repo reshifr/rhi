8a2efd979b1361c1919388917efa1fd1b1ea62ad
Renol P. H <reshifr@gmail.com>
2021-12-08 12:07:36 +0700

    fix: reset `main.cpp` to normal test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,11 @@
 #include "utils.hpp"
 
 int main(void) {
-  // TEST(1, set::unit_init());
-  // TEST(1, set::unit_reserve());
-  // TEST(1, set::unit_insert());
-  // TEST(1, set::unit_search());
-  // TEST(1, set::auto_insert());
-  utils::objs keys(3, 15, utils::AUTO_COUNT);
+  TEST(1, set::unit_init());
+  TEST(1, set::unit_reserve());
+  TEST(1, set::unit_insert());
+  TEST(1, set::unit_search());
+  TEST(1, set::auto_insert());
   TEST(1, set::perf_insert());
   return EXIT_SUCCESS;
 }
